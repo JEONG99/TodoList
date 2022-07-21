0a58e273b712f88b026f9590ddc1d9594764612e
JEONG99 <jbh990621@gmail.com>
2022-07-21 20:59:04 +0900

    todoList-fianl

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,34 @@
+import { useCallback, useState } from "react";
+import { MdAdd } from "react-icons/md";
+import "./TodoInsert.scss";
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState("");
+  const onChange = useCallback((event) => {
+    setValue(event.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      onInsert(value);
+      setValue("");
+    },
+    [onInsert, value]
+  );
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        value={value}
+        placeholder="할 일을 입력하세요."
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
