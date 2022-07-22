3a9bb1fdc2dad18ffe072c5f5723a475711ab91e
JEONG99 <jbh990621@gmail.com>
2022-07-22 18:55:29 +0900

    code-optimize

--- src/App.js
+++ src/App.js
@@ -5,53 +5,40 @@ import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리액트의 기초 알아보기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "컴포넌트 스타일링해 보기",
-      checked: false,
-    },
-    {
-      id: 3,
-      text: "일정 관리 앱 만들어 보기",
-      checked: false,
-    },
-  ]);
-
-  const nextId = useRef(4);
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text: text,
+  const createBulkTodos = () => {
+    const array = [];
+    for (let i = 1; i <= 2500; i++) {
+      array.push({
+        id: i,
+        text: `할 일 ${i}`,
         checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos]
-  );
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos]
-  );
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) => {
-          return todo.id === id ? { ...todo, checked: !todo.checked } : todo;
-        })
-      );
-    },
-    [todos]
-  );
+      });
+    }
+    return array;
+  };
+
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text: text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, checked: !todo.checked } : todo;
+      })
+    );
+  }, []);
 
   return (
     <TodoTemplate>
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -5,23 +5,29 @@ import {
 } from "react-icons/md";
 import cn from "classnames";
 import "./TodoListItem.scss";
+import React from "react";
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   const onClick = () => onRemove(id);
 
   return (
-    <div className="TodoListItem">
-      <div className={cn("checkbox", { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={onClick}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn("checkbox", { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={onClick}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
